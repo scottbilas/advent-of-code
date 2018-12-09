6535d78005e0391b08772872781e85eaecf54df8
Scott Bilas <scottbilas@gmail.com>
2018-12-09 14:24:41 +0100

    Validate against answers I typed in on web site
    Just in case I mess something up in in-script validation

--- 2018/test.ps1
+++ 2018/test.ps1
@@ -1,1 +1,16 @@
-dir -recur day*.linq | %{ write-host -nonew "Testing $($_.name)..."; lprun $_ }
+dir -Recurse day*.linq | %{
+    write-host -nonew "Testing $($_.Name)..."
+
+    $answers = cat $_.FullName.Replace(".linq", ".txt") |
+        ?{ $_ -match "Your puzzle answer was (.*)\." } |
+        %{ $matches[1] }
+
+    $results = lprun $_
+
+    if ([string]$results -eq [string]$answers) {
+        write-host ($results -join ', ')
+    }
+    else {
+        throw "Mismatch! Results: $results; Expected: $answers"
+    }
+}
