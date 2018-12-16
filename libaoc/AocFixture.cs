af2c6a22584f58e1bce709562b75c8f7038737e1
Scott Bilas <scottbilas@gmail.com>
2018-12-16 10:26:58 +0100

    Switch to dotnetcore to get latest latest

--- libaoc/AocFixture.cs
+++ libaoc/AocFixture.cs
@@ -4,5 +4,5 @@ using NUnit.Framework;
 [Parallelizable(ParallelScope.All)]
 public class AocFixture
 {
-    protected string ScriptDir => new NPath(TestContext.CurrentContext.TestDirectory).Parent.Parent.Combine(GetType().Name);
+    protected string ScriptDir => new NPath(TestContext.CurrentContext.TestDirectory).Combine("../../..", GetType().Name);
 }
