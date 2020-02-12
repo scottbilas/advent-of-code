6c35e438254bfa8260eaef593a808e6b951d690e
Scott Bilas <scottbilas@gmail.com>
2020-02-12 15:18:30 +0100

    2016 Day 1

--- 2016/aoc/day1/day1.solver.py
+++ 2016/aoc/day1/day1.solver.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 
-input = open('day1.input.txt').read().replace(',','').split()
+inputData = open('day1.input.txt').read().replace(',','').split()
+
 
 ### PART 1
 
-def solve1(moves):
+def solve1(moves, visitor=None):
     offsets = [(0, -1), (1, 0), (0, 1), (-1, 0)]
     x, y, dir = 0, 0, 0
     for move in moves:
         dir += 1 if move[0] == 'R' else -1
         offset = offsets[dir % 4]
-        dist = int(move[1:])
-        x += offset[0] * dist
-        y += offset[1] * dist
+        for i in range(int(move[1:])):
+            x, y = x + offset[0], y + offset[1]
+            if visitor and visitor(x, y):
+                return abs(x) + abs(y)
     return abs(x) + abs(y)
 
 # samples
@@ -23,14 +25,27 @@ assert solve1(['R5', 'L5', 'R5', 'R3']) == 12
 
 # problem
 
-s1 = solve1(input)
+s1 = solve1(inputData)
 print(s1)
 assert s1 == 146
 
 
 ### PART 2
 
+def solve2(moves):
+    visited = set()
+    def visitor(x, y):
+        if (x, y) in visited: return True
+        visited.add((x, y))
+        return False
+    return solve1(moves, visitor)
 
 # samples
 
+assert solve2(['R8', 'R4', 'R4', 'R8']) == 4
+
 # problem
+
+s2 = solve2(inputData)
+print(s2)
+assert s2 == 131
