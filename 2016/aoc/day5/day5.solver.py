2bba4b6d8bd3e29736183dff948bc76c7c61b2b0
Scott Bilas <scottbilas@gmail.com>
2020-02-12 23:03:15 +0100

    2016 Day 5 (bleh lame)

--- 2016/aoc/day5/day5.solver.py
+++ 2016/aoc/day5/day5.solver.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-input = open('day5.input.txt').read().strip()
-
-### PART 1
+def getinput(): return open('day5.input.txt').read().strip()
 
 import hashlib, itertools
+
+
+### PART 1
 
 def solve1(id):
     code = ''
@@ -17,7 +18,7 @@ def solve1(id):
 
 # samples
 
-assert solve1("abc") == '18f47a30'
+assert solve1('abc') == '18f47a30'
 
 # problem
 
@@ -28,8 +29,24 @@ assert s1 == '801b56a7'
 
 ### PART 2
 
+def solve2(id):
+    code, used = '_'*8, 0
+    for i in itertools.count():
+        hash = hashlib.md5((id + str(i)).encode('utf-8')).hexdigest()
+        if hash.startswith('00000'):
+            pos = int(hash[5], 16)
+            if pos < 8 and code[pos] == '_':
+                code = code[:pos] + hash[6] + code[pos+1:]
+                used += 1
+                if used == 8:
+                    return code
 
 # samples
 
+assert solve2('abc') == '05ace8e3'
 
 # problem
+
+s2 = solve2(getinput())
+print(s2)
+assert s2 == '424a0197'
