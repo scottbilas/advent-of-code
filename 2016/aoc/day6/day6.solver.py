55631ca13394e16ab3fe6a893ca9f3a9d4cd9ceb
Scott Bilas <scottbilas@gmail.com>
2020-02-13 08:35:36 +0100

    2016 Day 6

--- 2016/aoc/day6/day6.solver.py
+++ 2016/aoc/day6/day6.solver.py
@@ -1,50 +1,47 @@
 #!/usr/bin/env python
 
-input = open('day6.input.txt').read().splitlines()
+def getinput(): return open('day6.input.txt').read().splitlines()
 
+def getsample(): return [
+    'eedadn', 'drvtee', 'eandsr', 'raavrd',
+    'atevrs', 'tsrnev', 'sdttsa', 'rasrtv',
+    'nssdts', 'ntnada', 'svetve', 'tesnvt',
+    'vntsnd', 'vrdear', 'dvrsen', 'enarar']
 
-### PART 1
-
-def solve1(messages):
+def solve(messages, sort):
     ds = [{} for _ in range(len(messages[0]))]
     for message in messages:
         for i in range(len(message)):
             c, d = message[i], ds[i]
             d[c] = d.get(c, 0) + 1
-    return ''.join([sorted(d.items(), key=lambda kv: -kv[1])[0][0] for d in ds])
+    return ''.join([sorted(d.items(), key=lambda kv: sort*kv[1])[0][0] for d in ds])
+
+
+### PART 1
+
+def solve1(messages): return solve(messages, -1)
 
 # samples
 
-assert solve1([
-    'eedadn',
-    'drvtee',
-    'eandsr',
-    'raavrd',
-    'atevrs',
-    'tsrnev',
-    'sdttsa',
-    'rasrtv',
-    'nssdts',
-    'ntnada',
-    'svetve',
-    'tesnvt',
-    'vntsnd',
-    'vrdear',
-    'dvrsen',
-    'enarar',
-    ]) == 'easter'
+assert solve1(getsample()) == 'easter'
 
 # problem
 
-s1 = solve1(input)
+s1 = solve1(getinput())
 print(s1)
 assert s1 == 'afwlyyyq'
 
 
 ### PART 2
 
+def solve2(messages): return solve(messages, 1)
 
 # samples
 
+assert solve2(getsample()) == 'advent'
 
 # problem
+
+s2 = solve2(getinput())
+print(s2)
+assert s2 == 'bhkzekao'
