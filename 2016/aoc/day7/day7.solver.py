35e023dc6bdb9f1d1d4acb2936b10e5fa4099dd6
Scott Bilas <scottbilas@gmail.com>
2020-02-15 18:29:20 +0100

    2016 Day 7

--- 2016/aoc/day7/day7.solver.py
+++ 2016/aoc/day7/day7.solver.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-input = open('day7.input.txt').read().splitlines()
-
-
-### PART 1
+def getinput(): return open('day7.input.txt').read().splitlines()
 
 import re
+
+### PART 1
 
 def solve1(addresses):
 
@@ -20,8 +19,7 @@ def solve1(addresses):
         a = [0, 0]
         for (i, v) in enumerate([abba(v) for v in re.findall('\w+', address)]):
             a[i%2] += v
-        if a[0] != 0 and a[1] == 0:
-            count += 1
+        count += 1 if a[0] != 0 and a[1] == 0 else 0
     return count
 
 # samples
@@ -33,15 +31,38 @@ assert solve1(['ioxxoj[asdfgh]zxcvbn']) == 1
 
 # problem
 
-s1 = solve1(input)
+s1 = solve1(getinput())
 print(s1)
 assert s1 == 110
 
 
 ### PART 2
 
+def solve2(addresses):
+
+    def aba(s):
+        for i in range(2, len(s)):
+            if s[i-2] != s[i-1] and s[i-2] == s[i]:
+                yield s[i-2:i+1]
+
+    count = 0
+    for address in addresses:
+        abas = [set(), set()]
+        for (i, v) in enumerate([aba(v) for v in re.findall('\w+', address)]):
+            abas[i%2].update(v)
+        count += 1 if any(f'{v[1]}{v[0]}{v[1]}' in abas[1] for v in abas[0]) else 0
+
+    return count
 
 # samples
 
+assert solve2(['aba[bab]xyz']) == 1
+assert solve2(['xyx[xyx]xyx']) == 0
+assert solve2(['aaa[kek]eke']) == 1
+assert solve2(['zazbz[bzb]cdb']) == 1
 
 # problem
+
+s2 = solve2(getinput())
+print(s2)
+assert s2 == 242
