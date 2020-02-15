bb20f7c8882ac6f88f73f33d4876505ae67cebec
Scott Bilas <scottbilas@gmail.com>
2020-02-15 19:38:53 +0100

    2016 Day 9

--- 2016/aoc/day9/day9.solver.py
+++ 2016/aoc/day9/day9.solver.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
 
-input = open('day9.input.txt').read().strip()
-
-
-### PART 1
+def getinput(): return open('day9.input.txt').read().strip()
 
 import re
 
-def solve1(file):
-    length, i = 0, 0
-    while i < len(file):
-        if file[i] == '(':
-            end = file.index(')', i) + 1
-            take, repeat = [int(v) for v in re.findall('\d+', file[i:end])[:2]]
-            length += take * repeat
-            i = end + take
+def expand(str, start, end, nest):
+    length, i = 0, start
+    while i < end:
+        if str[i] == '(':
+            close = str.index(')', i) + 1
+            take, repeat = [int(v) for v in re.findall('\d+', str[i:close])]
+            length += nest(close, take) * repeat
+            i = close + take
         else:
             length += 1
             i += 1
     return length
+
+
+### PART 1
+
+def solve1(file): return expand(file, 0, len(file), lambda _, take: take)
 
 # samples
 
@@ -30,15 +32,27 @@ assert solve1('X(8x2)(3x3)ABCY') == 18
 
 # problem
 
-s1 = solve1(input)
+s1 = solve1(getinput())
 print(s1)
 assert s1 == 107035
 
 
 ### PART 2
 
+def solve2(file):
+    def nest(start, count):
+        return expand(file, start, start + count, nest)
+    return expand(file, 0, len(file), nest)
 
 # samples
 
+assert solve2('(3x3)XYZ') == len('XYZXYZXYZ')
+assert solve2('X(8x2)(3x3)ABCY') == len('XABCABCABCABCABCABCY')
+assert solve2('(27x12)(20x12)(13x14)(7x10)(1x12)A') == 241920
+assert solve2('(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN') == 445
 
 # problem
+
+s2 = solve2(getinput())
+print(s2)
+assert s2 == 11451628995
