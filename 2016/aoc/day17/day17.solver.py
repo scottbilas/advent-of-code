5d77f607a4ad69f76c250eb31a0fb314d0e4578b
Scott Bilas <scottbilas@gmail.com>
2020-04-20 17:08:48 +0200

    WIP

---
+++ 2016/aoc/day17/day17.solver.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+def getinput(): return open('day17.input.txt').read().strip()
+
+
+### PART 1
+
+# samples
+
+
+
+# problem
+
+#s1 = solve(getinput(), 272)
+#print(s1)
+#assert s1 == '10010110010011110'
+
+
+### PART 2
+
+# samples
+
+# problem
+
+#s2 = solve(getinput(), 35651584)
+#print(s2)
+#assert s2 == '01101011101100011'
--- 2016/aoc/day22/day22.solver.py
+++ 2016/aoc/day22/day22.solver.py
@@ -6,9 +6,9 @@ def getinput(): return open('day22.input.txt').read()
 
 import re
 
-def solve1():
+def solve1(text):
 
-    nums = [int(i) for i in re.findall(r'\d+', getinput())]
+    nums = [int(i) for i in re.findall(r'\d+', text)]
     avail = sorted(zip(nums[3::6], nums[4::6]), key=lambda v: (v[1], v[0]))
     used = [v for v in sorted(avail) if v[0] > 0]
     pairs, ia = 0, 0
@@ -22,13 +22,37 @@ def solve1():
 
     return pairs
 
-s1 = solve1()
+s1 = solve1(getinput())
 print(s1)
 assert s1 == 903
 
 
 ### PART 2
 
-#s2 = solve('fbgdceah', getinput(), True)
+def solve2(text):
+    nums = [int(i) for i in re.findall(r'\d+', text)]
+    grid = {}
+    for x, y, size, used in [tuple(nums[i:i+4]) for i in range(0, len(nums), 6)]:
+        grid[(x, y)] = (used, size)
+    start = (max(grid.keys())[0], 0)
+
+# sample
+
+assert solve2('''
+    Filesystem            Size  Used  Avail  Use%
+    /dev/grid/node-x0-y0   10T    8T     2T   80%
+    /dev/grid/node-x0-y1   11T    6T     5T   54%
+    /dev/grid/node-x0-y2   32T   28T     4T   87%
+    /dev/grid/node-x1-y0    9T    7T     2T   77%
+    /dev/grid/node-x1-y1    8T    0T     8T    0%
+    /dev/grid/node-x1-y2   11T    7T     4T   63%
+    /dev/grid/node-x2-y0   10T    6T     4T   60%
+    /dev/grid/node-x2-y1    9T    8T     1T   88%
+    /dev/grid/node-x2-y2    9T    6T     3T   66%
+    ''') == 7
+
+# problem
+
+#s2 = solve2(getinput())
 #print(s2)
-#assert s2 == 'aghfcdeb'
+#assert s2 ==
