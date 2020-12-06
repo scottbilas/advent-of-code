d9722be372c66320d1e57ad13b19188750798724
Scott Bilas <scottbilas@gmail.com>
2020-12-06 23:12:34 +0100

    New template

---
+++ 2020/aoc/_template/solver_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+)
+
+import . "scottbilas/advent-of-code/2020/libaoc"
+
+var input = ReadInputFile()
+
+var sample = NormalizeSample(`
+	`)
+
+// PART 1
+
+func solve1(source string) int {
+	total := -1
+	return total
+}
+
+func Test_Part1(t *testing.T) {
+	AssertEqualSub("Sample", t, solve1(sample), -1)
+	AssertEqualSub("Problem", t, solve1(input), -1)
+}
+
+func solve2(source string) int {
+	total := -1
+	return total
+}
+
+func Test_Part2(t *testing.T) {
+	AssertEqualSub("Sample", t, solve2(sample), -1)
+	AssertEqualSub("Problem", t, solve2(input), -1)
+}
