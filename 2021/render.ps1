8dfd8f27e808684c73aeb4573d14eeaa53f6be4a
Scott Bilas <scottbilas@gmail.com>
2021-12-03 00:23:41 +0100

    Bit of cleanup

--- 2021/render.ps1
+++ 2021/render.ps1
@@ -14,24 +14,23 @@ try {
         $day = $matches[1]
         Set-Location $_
         if (Get-Content "day$day.input.txt") {
+
             New-Item -force lightmode >$null
             $tempnb = "Day $day.ipynb"
             iee jupyter nbconvert --to notebook --execute --output $tempnb "day$day.solver.ipynb"
-            iee jupyter nbconvert --to pdf $tempnb
-            Remove-Item $tempnb
-            $pdf = "day$day.solver.pdf"
-            Remove-Item -ea:silent $pdf
-            Move-Item "Day $day.pdf" $pdf
             Remove-Item lightmode
 
+            iee jupyter nbconvert --to pdf $tempnb
+            Remove-Item $tempnb
+
             # yield path
-            Resolve-Path $pdf
+            Resolve-Path "Day $day.pdf"
         }
     }
-
-    iee pdfunite @pdfs "Advent of Code 2021.pdf"
-    Remove-Item $pdfs
 }
 finally {
     Pop-Location
 }
+
+iee pdfunite @pdfs "Advent of Code 2021.pdf"
+Remove-Item $pdfs
