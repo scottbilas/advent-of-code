71c6da0b6205ac45bd4ddd1efdcd6571535af35d
Scott Bilas <scottbilas@gmail.com>
2021-12-04 00:19:23 +0100

    New combined renderer

--- 2021/render.ps1
+++ 2021/render.ps1
@@ -5,32 +5,40 @@ $ErrorActionPreference = 'Stop'
 
 # setup:
 #
-# pip install jupyter nbconvert
-# scoop install latex pandoc poppler
+# scoop install python latex pandoc
+#
+# irm https://install.python-poetry.org | python -
+# ^^ don't use iwr, you get bytes instead of a string back from .Content (see https://stackoverflow.com/a/51334763/14582)
+# add '~\AppData\Roaming\Python\Scripts' to path
+#
+# cd advent-of-code/2021
+# poetry install
 
-Push-Location
 try {
-    $pdfs = Get-ChildItem "$PSScriptRoot\aoc" | Where-Object { $_ -match 'day(\d+)' } | ForEach-Object {
-        $day = $matches[1]
-        Set-Location $_
-        if (Get-Content "day$day.input.txt") {
+    . .venv\Scripts\activate.ps1
+    Push-Location $PSScriptRoot\aoc
+
+    $tempnbs = Get-ChildItem *.ipynb | Where-Object { $_ -match 'day(\d+)' } | ForEach-Object {
+        if (Get-Content "day$($matches[1]).results.txt") { # skip any incomplete/in-progress solutions
+            $tempnb = $_.fullname.replace('.solver.ipynb', '.temp.ipynb')
 
             New-Item -force lightmode >$null
-            $tempnb = "Day $day.ipynb"
-            iee jupyter nbconvert --to notebook --execute --output $tempnb "day$day.solver.ipynb"
-            Remove-Item lightmode
+            iee jupyter nbconvert --to notebook --execute --output $tempnb $_
+            Remove-Item -ea:silent lightmode
 
-            iee jupyter nbconvert --to pdf $tempnb
-            Remove-Item $tempnb
-
-            # yield path
-            Resolve-Path "Day $day.pdf"
+            Resolve-Path $tempnb
         }
     }
+
+    Set-Location ..
+    $merged = 'Advent of Code 2021.ipynb'
+    nbmerge @tempnbs > $merged
+    Remove-Item $tempnbs
+
+    iee jupyter nbconvert --to pdf $merged 'Advent of Code 2021.pdf'
+    Remove-Item $merged
 }
 finally {
     Pop-Location
+    deactivate
 }
-
-iee pdfunite @pdfs "Advent of Code 2021.pdf"
-Remove-Item $pdfs
