1b4b735a455ac592728a75890f72274d65af9710
Scott Bilas <scottbilas@gmail.com>
2022-12-09 00:02:55 +0100

    Get rid of Jest, too slow and more work to debug

--- 2022/aoc/utils.ts
+++ 2022/aoc/utils.ts
@@ -1,5 +1,25 @@
 import { readFileSync } from 'fs'
 
+function prettyMs(ms: number) {
+    // TODO: if ms > 1000, show seconds
+    return `${ms}ms`
+}
+
+export function test(name, solver, expected) {
+    const start = new Date().getTime();
+    const result = solver()
+    const end = new Date().getTime();
+
+    if (result === expected) {
+        console.log(`√ ${name} (${prettyMs(end - start)})`)
+    }
+    else {
+        console.log(`× ${name} (${prettyMs(end - start)})`)
+        console.log(`    Result: ${result}`)
+        console.log(`  Expected: ${expected}`)
+    }
+}
+
 export function getProblemInput(day: number) {
     return readFileSync(`aoc/day${day}.input.txt`, 'utf-8')
 }
