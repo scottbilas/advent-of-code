6a11863ddb3184be37663dcca90d3f1f6c59824e
Scott Bilas <scottbilas@gmail.com>
2023-01-21 00:35:44 +0100

    Added some parser timing and comments

--- 2022/aoc/day24.solver.ts
+++ 2022/aoc/day24.solver.ts
@@ -1,6 +1,8 @@
-import { getProblemInput, check, number2, parseStringGrid, CharGrid } from './utils'
+import { getProblemInput, check, number2, parseStringGrid, CharGrid, prettyMs } from './utils'
 
 const day = 24
+
+const startParse = new Date().getTime()
 
 const sample = parse(`
     #.######
@@ -12,12 +14,19 @@ const sample = parse(`
 
 const input = parse(getProblemInput(day))
 
+const endParse = new Date().getTime()
+
+console.log(`Parsed in ${prettyMs(endParse - startParse)}`)
+
 function parse(text): [CharGrid[], number, number] {
 
     let start = parseStringGrid(text)
     let innerSize = start.size.sub2([2, 2])
+
+    // all starting blizzard positions in order >v<^
     let allBlizzards: number2[][] = [[],[],[],[]]
 
+    // find all the blizzards, clearing the board as we go (we will clone it later)
     start.forEach((c, p) => {
         let o = '>v<^'.indexOf(c)
         if (o >= 0) {
@@ -26,15 +35,21 @@ function parse(text): [CharGrid[], number, number] {
         }
     })
 
+    // render for any given minute the blizzards and overlaps into a new CharGrid
     function render(minute): CharGrid {
         let grid = start.clone()
-        let offsets = [[1, 0], [0, 1], [innerSize.x-1, 0], [0, innerSize.y-1]]
+        let offsets = [[1, 0], [0, 1], [innerSize.x-1, 0], [0, innerSize.y-1]] // pre-offset positive for later mod
+
         for (let [idir, blizzards] of allBlizzards.entries()) {
             let offset = offsets[idir].map(v => v*minute)
+
+            // mostly for pretty printing, not strictly necessary to have the number progression..
             let next = {
                 '.': '>v<^'[idir],
                 '>': '2', 'v': '2', '<': '2', '^': '2',
                 '2': '3', '3': '4' }
+
+            // render the blizzard (or update count if already there)
             for (let blizzard of blizzards) {
                 let off = grid.offsetAt2(
                     (blizzard.x + offset[0]) % innerSize.x + 1,
@@ -42,12 +57,15 @@ function parse(text): [CharGrid[], number, number] {
                 grid.cells[off] = next[grid.cells[off]]
             }
         }
+
+        // convert into flat array for better perf later
         return CharGrid.fromStringGrid(grid)
     }
 
     let board: CharGrid[] = []
     let seen = new Set()
 
+    // we only need to render slices until the modulos line up and we start to cycle
     for (;;) {
         let slice = render(board.length)
         if (seen.has(slice.cells))
@@ -66,10 +84,14 @@ function solve(board: CharGrid[], minute: number, begin: number, end: number): n
     let nextWork = [begin]
     let offsets = [0, 1, board[0].cx, -1, -board[0].cx]
 
+    // simple BFS
+
     for (; nextWork.length; ++minute) {
         let work = nextWork
         nextWork = []
         let nextSlice = board[(minute+1) % board.length]
+
+        // could speed this up a little with a heuristic and priority queue
 
         for (let from of work) {
             for (let offset of offsets) {
