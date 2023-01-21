7d6f8efbdc09c4ba7b0e5f502449173786dd44f8
Scott Bilas <scottbilas@gmail.com>
2023-01-21 22:52:54 +0100

    Day 20

--- 2022/aoc/day20.solver.ts
+++ 2022/aoc/day20.solver.ts
@@ -2,132 +2,83 @@ import { getProblemInput, check, parseNumbers } from './utils'
 
 const day = 20
 
-const sample = parseNumbers(`
+const sample = `
     1
     2
     -3
     3
     -2
     0
-    4`)
-const input = parseNumbers(getProblemInput(day))
-
-function solve1(nums: number[]) {
-
-    let array = nums.slice()
-
-    for (let num of nums) {
-        let index = array.indexOf(num)
-        array.splice(index, 1)
-        index = (index + num) % array.length
-        array.splice(index, 0, num)
-    }
-
-    let result = 0
-
-    let index = array.indexOf(0)
-    for (let i = 0; i < 3; ++i) {
-        index = (index + 1000) % array.length
-        console.log(index, array[index])
-        result += array[index]
-    }
-
-    return result
-}
+    4`
+const input = getProblemInput(day)
 
 class Node {
-    constructor(public value: number, public prev: Node, public next: Node) {}
+    constructor(public value: number) {}
+    next: Node; prev: Node
 }
 
-function solve1x(nums) {
-    let head: Node
+function parse(text) {
+    let nodes = parseNumbers(text).map(n => new Node(n))
 
-    function toArray(start: Node): number[] {
-        let array = Array<number>()
-        let i = start
-        do {
-            array.push(i.value)
-            i = i.next
-        }
-        while (i != start)
-
-        return array
+    for (let i = 0; i < nodes.length; ++i) {
+        nodes[i].prev = nodes[i-1]
+        nodes[i].next = nodes[i+1]
     }
 
-    function find(start: Node, value: number): Node {
-        for (let i = start; ; i = i.next)
-            if (i.value == value)
-                return i
-    }
+    nodes[0].prev = nodes.at(-1)
+    nodes.at(-1).next = nodes[0]
 
-    head = new Node(nums[0], null, null)
-    head.next = head.prev = head
+    return nodes
+}
 
-    for (let i = 1; i < nums.length; ++i) {
-        let node = new Node(nums[i], head.prev, head)
-        head.prev.next = node
-        head.prev = node
-    }
-
-    for (let num of nums) {
-        if (num == 0)
+function mix(nodes: Node[]) {
+    for (let node of nodes) {
+        if (node.value == 0)
             continue
 
-        let i = find(head, num)
-//        i.prev.next = i.next
-//        i.next.prev = i.prev
-        let old = i
+        let count = node.value % (nodes.length-1)
+        if (node.value < 0)
+            count += nodes.length-1
 
-        if (num < 0)
-            --num
-        while (num < 0) {
-            i = i.prev
-            ++num
-        }
-        while (num > 0) {
-            i = i.next
-            --num
-        }
+        let dst = node
+        for (; count > 0; --count)
+            dst = dst.next
 
-        if (i == old)
-            continue
-
-        old.prev.next = old.next
-        old.next.prev = old.prev
-
-        old.next = i.next
-        old.prev = i
-        i.next.prev = old
-        i.next = old
-
-        //console.log(toArray(head))
+        node.prev.next = node.next // unlink
+        node.next.prev = node.prev
+        node.prev      = dst       // link inner
+        node.next      = dst.next
+        node.prev.next = node      // link outer
+        node.next.prev = node
     }
+}
 
-    let result = 0
-
-    let node = find(head, 0)
-    for (let i = 0; ; ++i, node = node.next) {
-        if (i == 1000 || i == 2000 || i == 3000) {
+function score(nodes: Node[]): number {
+    let node = nodes.find(n => n.value == 0)
+    for (let [i, result] = [0, 0]; ; ++i, node = node.next) {
+        if (i == 1000 || i == 2000)
             result += node.value
-            console.log(node.value)
-        }
-        if (i == 3000)
-            break
+        else if (i == 3000)
+            return result + node.value
     }
-
-    return result
 }
 
-function solve2(nums) {
+function solve1(text) {
+    let nodes = parse(text)
+    mix(nodes)
+    return score(nodes)
 }
 
-//check(`Day ${day}.1 Sample`,  () => solve1(sample), 3)
-check(`Day ${day}.1x Sample`,  () => solve1x(sample), 3)
-//check(`Day ${day}.1 Problem`, () => solve1(input),  -1)
-check(`Day ${day}.1x Problem`, () => solve1x(input),  -1)
-//check(`Day ${day}.2 Sample`,  () => solve2(sample), -1)
-//check(`Day ${day}.2 Problem`, () => solve2(input),  -1)
+function solve2(text) {
+    let nodes = parse(text)
+    for (let node of nodes)
+        node.value *= 811589153
+    for (let i = 0; i < 10; ++i)
+        mix(nodes)
+    return score(nodes)
+}
 
-//-8762 wrong
-//4729 too low
-//6248 too low
+check(`Day ${day}.1 Sample`,  () => solve1(sample),             3)
+check(`Day ${day}.1 Problem`, () => solve1(input),           7584)
+check(`Day ${day}.2 Sample`,  () => solve2(sample),    1623178306)
+check(`Day ${day}.2 Problem`, () => solve2(input),  4907679608191)
