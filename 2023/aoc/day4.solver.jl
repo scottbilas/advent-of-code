a3f750ebc79443b7c68eb1e3d9410af9fc3f0699
Scott Bilas <scottbilas@gmail.com>
2023-12-09 15:06:37 +0100

    nuthin

--- 2023/aoc/day4.solver.jl
+++ 2023/aoc/day4.solver.jl
@@ -4,7 +4,7 @@ using Chain
 
 input = getProblemInput()
 
-sample = getSampleLines(raw"""
+sample = getSampleLines("""
     Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
     Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
     Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
