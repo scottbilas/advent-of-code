21004719a661706becf007b066e14b5612bf71c8
Scott Bilas <scottbilas@gmail.com>
2024-12-03 16:02:08 +0000

    Cleanup around the edges

--- 2024/get.ps1
+++ 2024/get.ps1
@@ -1,9 +1,17 @@
 [CmdletBinding()]
 param (
-    [int]$Day
+    [int]$Day = [datetime]::now.day,
+    [int]$Year = [datetime]::now.year
 )
 
-# to update session: copy 'session' contents from edge://settings/cookies/detail?site=adventofcode.com into ~/.adventofcode.session
+# aoc requires 'session' contents from edge://settings/cookies/detail?site=adventofcode.com in ~/.adventofcode.session
 
-$aoc = Join-Path $PSScriptRoot ../Tools/aoc-cli/aoc.exe
-& $aoc download -d $Day -y 2024 -i $PSScriptRoot/aoc/day$($Day).input.txt -p $PSScriptRoot/aoc/day$($Day).results.md -m -o
+$aoc = "$PSScriptRoot/../Tools/aoc-cli/aoc.exe"
+$path = "$PSScriptRoot/aoc"
+$base = "$path/day$($Day)"
+
+& $aoc download -d $Day -y $Year -i "$base.input.txt" -p "$base.results.md" -m -o
+
+if (!(Test-Path "$base.solver.cs")) {
+    Copy-Item "$path/../solver.template" "$base.solver.cs"
+}
