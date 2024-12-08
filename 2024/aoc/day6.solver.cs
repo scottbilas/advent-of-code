aabd590f911938cfa624b287c055e0b7d92c35b8
Scott Bilas <scottbilas@gmail.com>
2024-12-08 23:02:50 +0000

    2024 Day 6

---
+++ 2024/aoc/day6.solver.cs
@@ -0,0 +1,37 @@
+class Day6 : Fixture
+{
+    (bool loop, IEnumerable<Int2> path) Solve(char[,] grid)
+    {
+        var pos = grid.Cells().Single(c => c.cell == '^').pos;
+        var seen = new HashSet<(Int2 pos, Dir dir)>();
+        var loop = true;
+
+        for (var dir = N; seen.Add((pos, dir)) && loop;)
+        {
+            var test = pos + dir.Move;
+            var next = grid.Get(test);
+            if (next == 'X')
+                loop = false;
+            else if (next == '#')
+                dir = dir.Next4;
+            else
+                pos = test;
+        }
+
+        return (loop, seen.Select(v => v.pos).Distinct());
+    }
+
+    char[,] Parse(string input) => input.ToGrid().AddBorder('X');
+
+    int Solve1(string input) => Solve(Parse(input)).path.Count();
+
+    int Solve2(string input) => With(Parse(input), grid => Solve(grid)
+        .path.Skip(1).AsParallel()
+        .Count(pos => Solve(grid.Copy().Set(pos, '#')).loop));
+
+    [Test] public void Sample1() => Solve1(SampleInput).ShouldBe(41);
+    [Test] public void Sample2() => Solve2(SampleInput).ShouldBe(6);
+
+    [Test] public void Part1() => Solve1(Input).ShouldBe(5461);
+    [Test] public void Part2() => Solve2(Input).ShouldBe(1836);
+}
